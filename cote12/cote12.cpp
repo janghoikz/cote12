cbf588da859203f1dd49df2725dbe3d8ea00e07a
ohne <ghdlr785@gmail.com>
2022-06-21 20:18:51 +0900

    초기 커밋.

---
+++ cote12/cote12.cpp
@@ -0,0 +1,21 @@
+﻿#include <iostream>
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(long long n) {
+    vector<int> answer;
+    while (n != 0)
+    {
+        answer.push_back(n % 10);
+        n /= 10;
+    }
+    return answer;
+}
+
+int main()
+{
+    std::cout << "Hello World!\n";
+}
+
